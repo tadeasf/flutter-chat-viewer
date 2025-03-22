bb2d0b7d1331d33099c211d68b07b8e0751e9e0c
tadeasf <business@tadeasfort.com>
2025-03-22 11:16:57 +0100

    fix: cors issues

--- lib/utils/web_image_viewer.dart
+++ lib/utils/web_image_viewer.dart
@@ -8,6 +8,8 @@ class WebImageViewer extends StatefulWidget {
   final BoxFit fit;
   final double? width;
   final double? height;
+  final bool useCorsProxy;
+  final String corsProxyUrl;
 
   const WebImageViewer({
     super.key,
@@ -15,6 +17,8 @@ class WebImageViewer extends StatefulWidget {
     this.fit = BoxFit.cover,
     this.width,
     this.height,
+    this.useCorsProxy = false,
+    this.corsProxyUrl = 'https://corsproxy.io/?',
     Widget loadingWidget = const CircularProgressIndicator(),
     Widget errorWidget = const Icon(Icons.error),
   });
@@ -32,8 +36,12 @@ class _WebImageViewerState extends State<WebImageViewer> {
     // Register a view factory with a plain img element
     // The key is this bypasses Flutter's HTTP client entirely
     ui_web.platformViewRegistry.registerViewFactory(_viewType, (int _) {
+      final String processedUrl = widget.useCorsProxy
+          ? '${widget.corsProxyUrl}${Uri.encodeComponent(widget.imageUrl)}'
+          : widget.imageUrl;
+
       final img = document.createElement('img') as HTMLImageElement
-        ..src = widget.imageUrl
+        ..src = processedUrl
         ..style.setProperty('object-fit', _getBrowserObjectFit(widget.fit))
         ..style.setProperty('width', '100%')
         ..style.setProperty('height', '100%');
